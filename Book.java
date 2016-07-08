dec44a5d9d3b587b6c15951e3323275c71511e35
LilyxxLin <lilyxxlin@gmail.com>
2016-07-08 17:35:39 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,5 +1,12 @@
 package WorldEconomy;
 
 public class Book extends Thing {
+  
+  //constructor
+  public Book(Double quantity){
+    super.name = "Book";
+    super.price = 5;
+    super.quantity = quantity;
+  }
 
 }
