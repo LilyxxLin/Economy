ba3244321b4e028194731e261b568ffd45fedb35
LilyxxLin <lilyxxlin@gmail.com>
2016-07-08 17:16:55 -0400

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,5 +1,12 @@
 package WorldEconomy;
 
 public class Car extends Thing{
-
+  
+  
+//constructor
+  public Car(Double quantity){
+    super.name = "Car";
+    super.price = 200;
+    super.quantity = quantity;
+  }
 }
