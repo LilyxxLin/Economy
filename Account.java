670c84de3f1eec671216667d2d76159a57fedf6f
LilyxxLin <lilyxxlin@gmail.com>
2016-07-08 17:24:49 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,13 +1,17 @@
 package WorldEconomy;
 
 public class Account {
-	private String ownerName;
 	private String accname;
+	private String ownerName;
+	private String bankName;
 	private double quantity;  // type currency??
 	
-	//accessor method
-	public double getQuantity(){
+	//accessor methods
+	public double getQuantity()
 		return quantity;
-	}
+	public String getAccName()
+		return accname;
+	public String getBankName()
+		return bankName;
 	
 }
