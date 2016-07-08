147d63bab6a7315eca0e9161466c5f955cb609e2
LilyxxLin <lilyxxlin@gmail.com>
2016-07-08 17:10:55 -0400

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Shop extends Entity {
 	private static int totalNumShop = 0;
-	private String name;
+	//private String name; inherited from Entity
 	private ArrayList<Thing> thingArray; // what about an arrayList of an arrayList???
 	private double totalAsset; // money or money+thing*Price
 	
@@ -16,8 +16,14 @@ public class Shop extends Entity {
 		return thingArray;
 	}
 	
-	public boolean isAvailable(Thing t, double amount){
-		return// the length of the arrayList of object that we are looking for
+	public boolean enoughItem(String thing, double quantity){
+		int itemIndex = super.getItemIndex (thing);
+		if (itemIndex>=0){  // if the item is found
+			return Thing[itemIndex].getQuantity()>=quantity;
+		}else { // if getItemIndex returns -1;
+			System.out.println(this.getName()+ " does not have the item that you are looking for... sorry :(");
+			return false; // or prompt the user to retype???
+		}
 	}
 	
 
