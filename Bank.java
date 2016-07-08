a9bfb2bf5ff1f99f8801b422d3358d5dea6d91ce
LilyxxLin <lilyxxlin@gmail.com>
2016-07-08 11:45:02 -0400

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,6 +1,6 @@
 package WorldEconomy;
 
-public class Bank inherits Entity{  //implements Account??  // implements transactions
+public class Bank extends Entity{  //implements Account??  // implements transactions
 	private static float totalMoney;
 
 	private People[] customers;
