11b0a1604efad675afd654a0adcdb03090807e15
LilyxxLin <lilyxxlin@gmail.com>
2016-07-08 17:18:04 -0400

    Update Carrot.java

--- Carrot.java
+++ Carrot.java
@@ -1,5 +1,11 @@
 package WorldEconomy;
 
 public class Carrot extends Thing{
-
+  
+//constructor
+  public Carrot(Double quantity){
+    super.name = "Carrot";
+    super.price = 2;
+    super.quantity = quantity;
+  }
 }
