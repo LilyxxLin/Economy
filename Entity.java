8538255aceb0305673595dd33dc34d03ebffceb7
LilyxxLin <lilyxxlin@gmail.com>
2016-07-09 17:07:28 -0400

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,8 +1,8 @@
 package WorldEconomy;
 import java.util.ArrayList;
 
-public class Entity 
-{  // do we want to make this class abstract
+public class Entity{ 
+  // do we want to make this class abstract
   private String name;
   //private double totalAsset;
   private double cash;
@@ -10,23 +10,23 @@ public class Entity
   public static final String[] DICTIONARY = {"car", "book", "carrot"};
   
   
-  public Entity()
-  {
+  public Entity(){
+  
     this.name = "";
     this.cash = 0.0;
     ArrayList<Account> = new ArrayList<Account>();  // or null??
 
   }
   
-  public Entity(String name, double cash)
-  {
+  public Entity(String name, double cash){
+  
       this.name = name;
       this.cash = cash;
       ArrayList<Account> = new ArrayList<Account>();  // or null??
   }
   
-  public int getItemIndex (String s) //returns index that corresponds to the string, and -1 if not found
-  {
+  public int getItemIndex (String s){ //returns index that corresponds to the string, and -1 if not found
+  
     for (int i = 0; i < DICTIONARY.length; i++)
       if(s[i].toLowerCase().equals(s))
         return i;
