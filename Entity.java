a2e13f5abf3c4680d021348f0c4be216930054bc
LilyxxLin <lilyxxlin@gmail.com>
2016-07-09 17:11:44 -0400

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -11,47 +11,37 @@ public class Entity{
   
   
   public Entity(){
-  
     this.name = "";
     this.cash = 0.0;
     ArrayList<Account> = new ArrayList<Account>();  // or null??
-
   }
-  
+  //constructor
   public Entity(String name, double cash){
-  
       this.name = name;
       this.cash = cash;
       ArrayList<Account> = new ArrayList<Account>();  // or null??
   }
   
   public int getItemIndex (String s){ //returns index that corresponds to the string, and -1 if not found
-  
     for (int i = 0; i < DICTIONARY.length; i++)
       if(s[i].toLowerCase().equals(s))
         return i;
     return -1;
   }
-  
+  //accessor
   public String getName(){
     return this.name;
   }
-  
   public double getCash(){
     return this.cash;
   }
   
-  public void addCash(double amount){
+  public void changeCash(double amount){
     cash += amount;
   }
   
-  public void subtractCash(double amount){
-    cash -= amount;
-  }
-  
   public void exchangeItem(String thing, double quantity){
-	  int itemIndex = super.getItemIndex (thing);
-	  this.Thing[itemIndex].changeQuantity(double quantity)
+    int itemIndex = super.getItemIndex (thing);
+	this.Thing[itemIndex].changeQuantity(double quantity)
 	}
-
 }
