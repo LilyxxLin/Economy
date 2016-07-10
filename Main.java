d52ff5971967a0b603b83389cd6fa319b8061f91
LilyxxLin <lilyxxlin@gmail.com>
2016-07-10 11:32:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ public class Main {
   public static void main (String[] args){
     //construct the initial setup
     setup();
-    Account aliceAcc1 = alice.openAccount (TompkinsTrustCompany, "aliceAccount1", 800);
+    Account aliceAcc1 = alice.openAccount (TompkinsTrustCompany, "aliceAcc1", 800);
     lily.buy (BearNecessities, "Carrot", 50, null, null); //toString: lily (uses cash) buys 50 carrots from BearNecessity (reveices cash)
     Account lucasAcc1 = lucas.openAccount (TompkinsTrustCompany, "LucasAcc1", 100);
     alice.buy (lily, "Book", 2, aliceAccount1, null, aliceAcc1);  // should return error message...
     lucas.deposit (lucasAcc1, 300); //toString should print out amount of money before and after deposit
+    lucas.buy (BearNecessitties, "Car", 2, lucasAcc1, )
     
     
     
