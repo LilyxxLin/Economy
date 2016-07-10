ba27e56afc04daee4e26e1a6ff8915cb051d8c04
alicexinyun <xc278@cornell.edu>
2016-07-10 11:09:11 -0400

    Update Thing.java

--- Thing.java
+++ Thing.java
@@ -1,27 +1,25 @@
 package WorldEconomy;
 
-public class Thing {
+public class Thing{
 	private String name;
 	private double price;
 	private int quantity;
 	
-	//constructor
-	public Thing(String n, double p, int q){
-		name = n;
-		price = p;
-		quantity = q;
-	}
-    //accesor methods
-	public double getPrice(){
-		return price;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	//change the quantity of the item in the list
-	public void changeQuantity(double quantityChanged){
-		quantity = quantity + quantityChanged;
+	public Thing(String n, double p, int q){ // constructor
+		this.name = n;
+		this.price = p;
+		this.quantity = q;
 	}
 
+	public double getPrice(){ // accessor
+		return this.price;
 	}
+	public int getQuantity() {
+		return this.quantity;
+	}
+	//change the quantity of the item in the list
+	public void changeQuantity(int quantityChanged){
+		this.quantity += quantityChanged;
+	}
+
 }
