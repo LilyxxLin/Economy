bce9811f31cc8460a2eff3c9607cbdccf3955927
alicexinyun <xc278@cornell.edu>
2016-07-10 11:31:29 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -2,25 +2,31 @@ package WorldEconomy;
 
 public class Account {
 	private int idNumber;
+	private String accName;
 	private String ownerName;
 	private String bankName;
 	private double balance;  // type currency??? default unit: US dollar
 
 	public Account(int id, String accn, String ownern, String bankn, double initialDeposit){ // constructor
 		this.idNumber = id;
+		this.accName = accn;
 		this.ownerName = ownern;
 		this.bankName = bankn;
 		this.balance = initialDeposit;
 	}
 	public Account(int id, String accn, String ownern, String bankn){ // default constructor
 		this.idNumber = id;
+		this.accName = accn;
 		this.ownerName = ownern;
 		this.bankName = bankn;
 		this.balance = 0.0;
 	}
 
 	public int getIDNumber(){ //accessor
-		return this.idNumber
+		return this.idNumber;
+	}
+	public String getAccName(){
+		return this.accName;
 	}
 	public String getOwnerName(){
 		return this.ownerName;
