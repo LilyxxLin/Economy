eacd566a144ad204ebb44197eb79aa4a746da625
lucaschen321 <lmc336@cornell.edu>
2016-07-10 16:27:35 -0400

    Delete Money.java

--- Money.java
+++
@@ -1,9 +0,0 @@
-package WorldEconomy;
-
-public class Money { //We can put in currencies: different types of money afterwards
-	private static float totalInSystem;
-	private float amount;
-	public Money(float value){
-		
-	}
-}
