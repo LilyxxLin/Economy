41a48b916e1a8ca9d37d9e37038a18ce5eec12f6
LilyxxLin <lilyxxlin@gmail.com>
2016-07-10 10:36:06 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 package WorldEconomy;
 
 public class Main {
+  public static void main (String[] args){
+    //construct the initial setup
+    setup();
+    
+  }
+  
+  public static void setup (){
+    Person alice = new Person ("Alice", 1000);
+    Person lucas = new Person ("Lucas", 1000);
+    Person lily = new Person("Lily", 1000);
+    Shop shop1 = new Shop ("Bear Necessity");
+    Bank bank1 = new Bank ("Tompkins Trust Company");
+  }
 
 }
