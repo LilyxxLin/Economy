505a9468a4812d7873e6a329db612d3bd245b5d7
alicexinyun <xc278@cornell.edu>
2016-07-10 11:29:34 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -6,23 +6,20 @@ public class Account {
 	private String bankName;
 	private double balance;  // type currency??? default unit: US dollar
 
-	// constructor
-	public Account(int id, String accn, String ownern, String bankn, double initialDeposit){
+	public Account(int id, String accn, String ownern, String bankn, double initialDeposit){ // constructor
 		this.idNumber = id;
 		this.ownerName = ownern;
 		this.bankName = bankn;
 		this.balance = initialDeposit;
 	}
-	// default constructor
-	public Account(int id, String accn, String ownern, String bankn){
+	public Account(int id, String accn, String ownern, String bankn){ // default constructor
 		this.idNumber = id;
 		this.ownerName = ownern;
 		this.bankName = bankn;
 		this.balance = 0.0;
 	}
 
-	//accessors
-	public int getIDNumber(){
+	public int getIDNumber(){ //accessor
 		return this.idNumber
 	}
 	public String getOwnerName(){
@@ -34,5 +31,13 @@ public class Account {
 	public double getBalance(){
 		return this.balance;
 	}
-
+	
+	public void transferTo(Account toAcc, double quantity){
+		if(this.balance < quantity){
+			System.out.println("Insufficient balance; transfer unsuccessful.");
+		} else{
+			this.balance -= quantity; // password??? method changeBalance()
+			toAcc.balance += quantity;
+		}
+	}
 }
