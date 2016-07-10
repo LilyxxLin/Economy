b134e4aaa408c65d12f1cad7fca086713716ded9
LilyxxLin <lilyxxlin@gmail.com>
2016-07-10 11:07:19 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,13 @@ public class Main {
   public static void main (String[] args){
     //construct the initial setup
     setup();
+    Account aliceAcc1 = alice.openAccount (TompkinsTrustCompany, "aliceAccount1", 800);
+    lily.buy (BearNecessities, "Carrot", 50, null, null); //toString: lily (uses cash) buys 50 carrots from BearNecessity (reveices cash)
+    Account lucasAcc1 = lucas.openAccount (TompkinsTrustCompany, "LucasAcc1", 100);
+    alice.buy (lily, "Book", 2, aliceAccount1, null, aliceAcc1);  // should return error message...
+    lucas.deposit (lucasAcc1, 300); //toString should print out the current account
+    
+    
     
   }
   
@@ -11,8 +18,10 @@ public class Main {
     Person alice = new Person ("Alice", 1000);
     Person lucas = new Person ("Lucas", 1000);
     Person lily = new Person("Lily", 1000);
-    Shop shop1 = new Shop ("Bear Necessity");
-    Bank bank1 = new Bank ("Tompkins Trust Company");
+    Shop BearNecessities = new Shop ("Bear Necessities");
+    Bank TompkinsTrustCompany = new Bank ("Tompkins Trust Company");
   }
+  
+  public static 
 
 }
