91d52bfd720bc97eefe8d912903d13ac54403a70
alicexinyun <xc278@cornell.edu>
2016-07-10 10:20:08 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -6,21 +6,6 @@ public class Account {
 	private String bankName;
 	private double balance;  // type currency??? default unit: US dollar
 
-	//accessors
-	public int getIDNumber(){
-		return idNumber
-	}
-
-	public String getOwnerName(){
-		return ownerName;
-	}
-	public String getBankName(){
-		return bankName;
-	}
-	public double getBalance(){
-		return balance;
-	}
-	
 	// constructor
 	public Account(int id, String accn, String ownern, String bankn, double initialDeposit){
 		idNumber = id;
@@ -34,6 +19,21 @@ public class Account {
 		ownerName = ownern;
 		bankName = bankn;
 		balance = 0.0;
+	}
+
+	//accessors
+	public int getIDNumber(){
+		return idNumber
+	}
+
+	public String getOwnerName(){
+		return ownerName;
+	}
+	public String getBankName(){
+		return bankName;
+	}
+	public double getBalance(){
+		return balance;
 	}
 
 }
